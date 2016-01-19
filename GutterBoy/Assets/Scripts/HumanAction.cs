ea0ffa9c4bc2ffd4c5a4d44d76263298f0534004
Gurigura <gurigura789@gmail.com>
2016-01-19 18:13:56 +0900

    [処理の追加]HumanAction.csの追加、その他いろいろ調整

---
+++ GutterBoy/Assets/Scripts/HumanAction.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HumanAction : MonoBehaviour {
+
+	[SerializeField]
+	private float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move ();
+	}
+
+	void Move(){
+		this.gameObject.transform.Translate (0, 0, speed);
+	}
+
+	void OnTriggerEnter(Collider col){
+		if (col.gameObject.tag == "Exit") {
+			Destroy (this.gameObject);
+		}
+
+	}
+}
